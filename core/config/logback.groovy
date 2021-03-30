a3a0e6e39faad01d8b4c0ea1151f6b8e14fcad5c
LifeWeaver <jmdearing_@hotmail.com>
2021-03-30 17:16:03 -0400

    Add logback

---
+++ core/config/logback.groovy
@@ -0,0 +1,33 @@
+package net.stardecimal.game.config
+
+import ch.qos.logback.core.*
+import ch.qos.logback.core.encoder.*
+import ch.qos.logback.core.read.*
+import ch.qos.logback.core.rolling.*
+import ch.qos.logback.core.status.*
+import ch.qos.logback.classic.net.*
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+
+statusListener(OnConsoleStatusListener)
+
+appender("FILE", FileAppender) {
+	file = "../../log/main.log"
+	append = true
+	encoder(PatternLayoutEncoder) {
+		pattern = "%d %level %logger{5} - %msg%n"
+	}
+	rollingPolicy(TimeBasedRollingPolicy) {
+		FileNamePattern = "/log/main-%d{yyyy-MM}.zip"
+	}
+
+}
+
+
+appender("STDOUT", ConsoleAppender) {
+
+}
+
+
+
+root(INFO, ["FILE", "STDOUT"])
+scan()
