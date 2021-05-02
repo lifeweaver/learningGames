dc228ca7a38c3ca3a44627e717c43fceccf51989
LifeWeaver <jmdearing_@hotmail.com>
2021-05-02 14:02:31 -0400

    Only spawn one bomber plane at a time, remove old bomber planes if they are off screen.

--- core/src/net/stardecimal/game/missilecommand/entity/systems/EnemySystem.groovy
+++ core/src/net/stardecimal/game/missilecommand/entity/systems/EnemySystem.groovy
@@ -1,6 +1,12 @@
 package net.stardecimal.game.missilecommand.entity.systems
 
+import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IntervalSystem
+import com.badlogic.gdx.math.Vector2
+import net.stardecimal.game.entity.components.Mapper
+import net.stardecimal.game.entity.components.SdBodyComponent
+import net.stardecimal.game.entity.components.TypeComponent
+import net.stardecimal.game.entity.systems.RenderingSystem
 import net.stardecimal.game.missilecommand.LevelFactory
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -27,9 +33,33 @@ class EnemySystem extends IntervalSystem {
 		}
 
 		//Spawn a bomber plane every 10 missiles
-//		if(missilesSpawned % 10 == 0) {
-			levelFactory.createBomberPlane()
-//		}
+		if(missilesSpawned % 10 == 0) {
+			Vector2 screenSize = RenderingSystem.getScreenSizeInMeters()
+			float maxX = screenSize.x / RenderingSystem.PPM as float
+			float maxY = screenSize.y / RenderingSystem.PPM as float
+			List<Entity> bomberPlanes = engine.getEntities().findAll {
+				Mapper.typeCom.get(it)?.type == TypeComponent.TYPES.BOMBER_PLANE
+			}
+
+			//Kill any bomber planes outside the screen
+			bomberPlanes.each {
+				SdBodyComponent sdBody = Mapper.bCom.get(it)
+				Vector2 pos = sdBody.body.position
+				if(pos.x < 0 || pos.y < 0 || pos.x > maxX || pos.y > maxY) {
+					sdBody.isDead = true
+				}
+			}
+
+			//Only leave alive bomber planes in list
+			bomberPlanes.removeAll {
+				Mapper.bCom.get(it).isDead
+			}
+
+			//Don't spawn a bomber plane if one already exists
+			if(bomberPlanes.isEmpty()) {
+				levelFactory.createBomberPlane()
+			}
+		}
 
 		//Spawn a satellite plane every 15 missiles
 		if(missilesSpawned % 15 == 0) {
