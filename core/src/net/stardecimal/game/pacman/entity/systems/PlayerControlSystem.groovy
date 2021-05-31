d5e5ed458e4e4629cdb06e92f6161978df5a3ca6
LifeWeaver <jmdearing_@hotmail.com>
2021-05-31 13:00:34 -0400

    The player should change velocity if their intended direction is blocked.

--- core/src/net/stardecimal/game/pacman/entity/systems/PlayerControlSystem.groovy
+++ core/src/net/stardecimal/game/pacman/entity/systems/PlayerControlSystem.groovy
@@ -3,6 +3,7 @@ package net.stardecimal.game.pacman.entity.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.math.Vector2
 import net.stardecimal.game.KeyboardController
 import net.stardecimal.game.entity.components.Mapper
 import net.stardecimal.game.entity.components.PlayerComponent
@@ -24,25 +25,48 @@ class PlayerControlSystem extends IteratingSystem {
 	protected void processEntity(Entity entity, float deltaTime) {
 		SdBodyComponent playerBody = Mapper.bCom.get(entity)
 
-		if (controller.left) {
+		if (controller.left && shouldChangeVelocity(playerBody)) {
 			playerBody.body.setLinearVelocity(-3f, 0)
 			Mapper.transCom.get(entity).rotation = 0
 		}
 
-		if (controller.right) {
+		if (controller.right && shouldChangeVelocity(playerBody)) {
 			playerBody.body.setLinearVelocity(+3f, 0)
 			Mapper.transCom.get(entity).rotation = 180
 		}
 
-		if (controller.up) {
+		if (controller.up && shouldChangeVelocity(playerBody)) {
 			playerBody.body.setLinearVelocity(0, +4f)
 			Mapper.transCom.get(entity).rotation = 270
 		}
 
-		if (controller.down) {
+		if (controller.down && shouldChangeVelocity(playerBody)) {
 			playerBody.body.setLinearVelocity(0, -4f)
 			Mapper.transCom.get(entity).rotation = 90
 		}
+	}
+
+	boolean shouldChangeVelocity(SdBodyComponent body) {
+		Vector2 tilePos = levelFactory.tilePosition(body.body.position.x, body.body.position.y)
+		switch (true) {
+			case controller.left:
+				tilePos.x = tilePos.x - 1 as float
+				break
+
+			case controller.right:
+				tilePos.x = tilePos.x + 1 as float
+				break
+
+			case controller.up:
+				tilePos.y = tilePos.y + 1 as float
+				break
+
+			case controller.down:
+				tilePos.y = tilePos.y - 1 as float
+				break
+		}
+
+		return !levelFactory.isCellBlocked(tilePos)
 	}
 
 }
