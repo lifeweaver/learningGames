e34484400f29ccdcabfd436df5dfb1a36f14e033
LifeWeaver <jmdearing_@hotmail.com>
2021-06-25 21:48:07 -0400

    Update map.

--- core/assets/maps/background.tsx
+++ core/assets/maps/background.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="background" tilewidth="1280" tileheight="768" tilecount="1" columns="0">
+<tileset version="1.5" tiledversion="1.6.0" name="background" tilewidth="1280" tileheight="768" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
   <image width="1280" height="768" source="png/BG/BG_2.png"/>
+ </tile>
+ <tile id="3">
+  <image width="500" height="500" source="png/Object/pancake.png"/>
  </tile>
 </tileset>
